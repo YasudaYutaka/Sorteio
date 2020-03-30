c62907e949c979cb5612a2b9b59d4d5c1f70f1ff
HenriqueAkira <62554340+YasudaYutaka@users.noreply.github.com>
2020-03-30 15:26:08 -0300

    Switch para repetir / AlertDialog Message

---
+++ app/src/main/java/com/yutaka/sorteio/DialogoResultado.java
@@ -0,0 +1,33 @@
+package com.yutaka.sorteio;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class DialogoResultado extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Resultado")
+                .setMessage(resultadoTeste)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
+    }
+
+
+    public String resultadoTeste;
+
+}
+
+
--- app/src/main/java/com/yutaka/sorteio/MainActivity.java
+++ app/src/main/java/com/yutaka/sorteio/MainActivity.java
@@ -7,34 +7,72 @@ import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Switch repetirSwitch;
+    private TextView quantidadeRepet;
+    DialogoResultado dialogoResultado = new DialogoResultado(); // Chamando Classe DialogoResultado
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
+
+        repetirSwitch = findViewById(R.id.switchRep);
+        quantidadeRepet = findViewById(R.id.editTextNumRepet);
+
+        adicionarListener(); // Listener do Switch
     }
+
+    public void openDialog(){  // METODO QUE MOSTRA MENSAGEM
+        dialogoResultado.show(getSupportFragmentManager(), "teste");
+    }
+
+
+
+    public void adicionarListener(){  // LISTENER DO SWITCH
+        repetirSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    quantidadeRepet.setVisibility(View.VISIBLE);
+                } else {
+                    quantidadeRepet.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
 
     public void sortearNum(View view) {
 
-        TextView quantidadeNum1 = findViewById(R.id.editTextMenorNum);
-        TextView quantidadeNum2 = findViewById(R.id.editTextMaiorNum);
-        String verificador1 = quantidadeNum1.getText().toString();
-        String verificador2 = quantidadeNum2.getText().toString();
-        ImageView imagemCirculo = findViewById(R.id.imageCircle);
+       TextView quantidadeNum1 = findViewById(R.id.editTextMenorNum);
+       TextView quantidadeNum2 = findViewById(R.id.editTextMaiorNum);
+       String verificador1 = quantidadeNum1.getText().toString();
+       String verificador2 = quantidadeNum2.getText().toString();
 
-        TextView texto1 = findViewById(R.id.txtMsg);
-        TextView texto2 = findViewById(R.id.editTextRes);
-        imagemCirculo.setVisibility(View.GONE);
-        texto2.setVisibility(View.GONE);
+       ImageView imagemCirculo = findViewById(R.id.imageCircle);
 
+       TextView texto1 = findViewById(R.id.txtMsg);
+       TextView texto2 = findViewById(R.id.editTextRes);
+       imagemCirculo.setVisibility(View.GONE);
+       texto1.setVisibility(View.VISIBLE);
+       texto2.setVisibility(View.GONE);
 
-        quantidadeNum2.onEditorAction(EditorInfo.IME_ACTION_DONE); // esconde teclado ao clicar no botão do programa
+       String verificador3 = quantidadeRepet.getText().toString();
+
+       quantidadeNum2.onEditorAction(EditorInfo.IME_ACTION_DONE); // esconde teclado ao clicar no botão do programa
+       Boolean switchState = repetirSwitch.isChecked();
 
         if (verificador1.equals("") || verificador2.equals("")){  // verifica se o usuário digitou número
             texto1.setText("Digite um número!");
@@ -44,13 +82,37 @@ public class MainActivity extends AppCompatActivity {
             if(numeroUsuario1 > numeroUsuario2){          // verifica se o meanor número é menor do que o maior número
                 texto1.setText("Digite um intervalo válido");
             }else {
-                Random random = new Random();
-                int numeroSorteado = random.nextInt((numeroUsuario2 - numeroUsuario1)+1)+numeroUsuario1;  // gera número aleatório de acordo com o Intervado
-                texto1.setText("O número sorteado é:");
-                texto2.setText("" + numeroSorteado);
-                texto2.setVisibility(View.VISIBLE);
-                imagemCirculo.setVisibility(View.VISIBLE);
+                if (switchState == true) {
+                    if (verificador3.equals("")){
+                        texto1.setText("Digite a quantidade de repetições");
+                    } else {
+                        dialogoResultado.resultadoTeste = "";
+                        int contador = 0;
+                        int numeroUsuario3 = Integer.parseInt(quantidadeRepet.getText().toString());
+                        int[] numSorteados = new int[numeroUsuario3];
+                        texto1.setVisibility(View.GONE);
+                        Random random = new Random();
+                        do {
+                            int numeroSorteado = random.nextInt((numeroUsuario2 - numeroUsuario1) + 1) + numeroUsuario1;  // gera número aleatório de acordo com o Intervado
+                            numSorteados[contador] = numeroSorteado;
+                            contador++;
+                            dialogoResultado.resultadoTeste += contador +"º resultado é: "+numeroSorteado+"\n"; // DEFINE ESCRITA MENSAGEM
+                            // FALTA GUARDAR
+                        } while (contador < numeroUsuario3);
+                        openDialog(); // ABRE ABA MENSAGEM
+                    }
+
+                } else {
+                    Random random = new Random();
+                    int numeroSorteado = random.nextInt((numeroUsuario2 - numeroUsuario1)+1)+numeroUsuario1;
+                    texto1.setText("O número sorteado é:");
+                    texto2.setText("" + numeroSorteado);
+                    texto2.setVisibility(View.VISIBLE);
+                    imagemCirculo.setVisibility(View.VISIBLE);
+                }
+
             }
         }
+
     }
 }
